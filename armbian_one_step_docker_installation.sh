3fcd3bfeb484d0ccde17f768b8b2104020228c39
铃木和花 <49331402+SuzukiHonoka@users.noreply.github.com>
2019-07-28 22:19:33 +0800

    Update armbian_one_step_docker_installation.sh

--- armbian_one_step_docker_installation.sh
+++ armbian_one_step_docker_installation.sh
@@ -4,5 +4,5 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh --mirror Aliyun
 #
 docker volume create portainer_data
-docker run --restart=always -d -p 7000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer:linux-arm64
+docker run --restart=always -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer:linux-arm64
 echo '脚本结束。管理地址IP:9000'
