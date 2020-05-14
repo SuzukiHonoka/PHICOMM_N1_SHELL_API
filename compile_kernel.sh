31db006226470fd85a8bb3ec2c5dfad201a6c1c3
铃木和花 <49331402+SuzukiHonoka@users.noreply.github.com>
2020-05-14 22:11:12 +0800

    Update compile_kernel.sh

--- compile_kernel.sh
+++ compile_kernel.sh
@@ -5,4 +5,4 @@ tar -xf gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar -C gcc-aarch64
 rm gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu*
 cd gcc-aarch64
 wget https://github.com/SuzukiHonoka/PHICOMM_N1_SHELL_API/raw/master/.config
-make -j$(nproc)
+make -j$(nproc) LOCALVERSION="-aml-s9xxx-starx" bindeb-pkg
