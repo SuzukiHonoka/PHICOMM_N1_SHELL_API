16228f21cf1746ca459211cb383eb7700924e0a3
铃木和花 <49331402+SuzukiHonoka@users.noreply.github.com>
2020-08-01 22:37:59 +0800

    some improvement

--- setup_new_kernel.sh
+++ setup_new_kernel.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+ARG1=$1
 BUILD_DIR="/tmp"
 REPO="Amlogic_s905-kernel"
 SRCURL="https://github.com/SuzukiHonoka/$REPO"
-KVER="linux-5.7.11"
+KVERV="5.7.12"
+if [ -z "$ARG1" ]; then
+KVERV=$ARG1
+fi
+KVER="linux-$KVERV"
 KURL="https://cdn.kernel.org/pub/linux/kernel/v5.x"
 KDURL="$KURL/$KVER.tar.xz"
 cd $BUILD_DIR
@@ -18,7 +23,13 @@ cd "$BUILD_DIR/$REPO"
 rsync -av --progress $BUILD_DIR/$KVER/* .
 sed -i "s/0x00080000/0x01080000/g" arch/arm64/Makefile
 sed -i "s/#error TEXT_OFFSET must be less than 2MB/\/\/#error TEXT_OFFSET must be less than 2MB/g" arch/arm64/kernel/head.S
+cat > arch/arm64/boot/dts/amlogic/meson-gxl-s905d-phicomm-n1.dts <<EOF
+&external_phy {
+        /delete-property/ reset-gpios;
+        interrupts = <25 IRQ_TYPE_LEVEL_LOW>;
+};
+EOF
 git status
 git add .
-git commit -m "update kernel to $KVER"
+git commit -m "Update Kernel to $KVER"
 git push
