edbfc9ae7a156b28b0e7ee883a981c7734b3d962
铃木和花 <49331402+SuzukiHonoka@users.noreply.github.com>
2020-12-20 15:18:12 +0800

    use patch instead hard cored text.

--- setup_new_kernel.sh
+++ setup_new_kernel.sh
@@ -31,12 +31,3 @@ xz -d "$KVER.tar.xz" && rm "$KVER.tar.xz"
 tar xf "$KVER.tar" && rm "$KVER.tar"
 cd "$BUILD_DIR/$KVER"
 #curl -o .config https://github.com/SuzukiHonoka/s905d-kernel-precompiled/raw/master/.config
-
-sed -i "s/TEXT_OFFSET := 0x0/TEXT_OFFSET := 0x01080000/g" arch/arm64/Makefile
-sed -i "s/#error TEXT_OFFSET must be less than 2MB/\/\/#error TEXT_OFFSET must be less than 2MB/g" arch/arm64/kernel/head.S
-cat >> arch/arm64/boot/dts/amlogic/meson-gxl-s905d-phicomm-n1.dts <<EOF
-&external_phy {
-        /delete-property/ reset-gpios;
-        interrupts = <25 IRQ_TYPE_LEVEL_LOW>;
-};
-EOF
