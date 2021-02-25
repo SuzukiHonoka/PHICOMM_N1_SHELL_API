8039757a7a7833128bc1edc59ddbf73ac8c70907
铃木和花 <49331402+SuzukiHonoka@users.noreply.github.com>
2021-02-25 20:12:37 +0800

    Update setup_aarch64_toolchain.sh

--- setup_aarch64_toolchain.sh
+++ setup_aarch64_toolchain.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 echo "Starting aarch64 toolchain setup"
+stage_dir="/opt/toolchain/aarch64"
 tool_url="https://github.com/SuzukiHonoka/s905d-kernel-precompiled/releases/download/gcc-10.2/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu.tar.xz"
-mkdir -p /tmp/stage
-cd /tmp/stage
+mkdir -p $stage_dir
+cd $stage_dir
 wget $tool_url
 xz -d gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu.tar.xz
 tar xf gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu.tar
- echo 'PATH=$PATH:/tmp/ramfs/toolchain/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin' > ~/.bashrc
+echo "AARCH64_TOOLCHAIN_DIR=$stage_dir" >> ~/.bashrc
+echo 'PATH=$PATH:$AARCH64_TOOLCHAIN_DIR/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin' >> ~/.bashrc
 source ~/.bashrc
 echo "Done"
