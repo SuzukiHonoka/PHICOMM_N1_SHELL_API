af96b2ecfaa4ee1cd2314ba0374757a6de58a3b2
铃木和花 <49331402+SuzukiHonoka@users.noreply.github.com>
2021-05-05 10:55:22 +0800

    optimize

--- setup_aarch64_toolchain.sh
+++ setup_aarch64_toolchain.sh
@@ -6,7 +6,7 @@ mkdir -p $stage_dir
 cd $stage_dir
 wget $tool_url
 xz -d gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu.tar.xz
-tar xf gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu.tar
+tar xf gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu.tar && rm gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu.tar
 echo "AARCH64_TOOLCHAIN_DIR=$stage_dir" >> ~/.bashrc
 echo 'PATH=$PATH:$AARCH64_TOOLCHAIN_DIR/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin' >> ~/.bashrc
 source ~/.bashrc
